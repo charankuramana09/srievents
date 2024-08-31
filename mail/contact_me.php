408e72a2c65836ae14d350902507afcd47ee0e7d
pavaniartech <pavani.ar@isigntech.com>
2024-08-31 18:20:59 +0530

    changes

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $subject = ($_POST['subject'] ? $_POST['subject'] : "Website Contact Form: $name
 
 
 // Create the email and send the message
-$to = 'pavaniar159@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@srisportsevents.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = $subject;
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nLast Name: $lastname\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: srisportsevents@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: info@srisportsevents.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 echo json_encode(array('success'=>'true'));
